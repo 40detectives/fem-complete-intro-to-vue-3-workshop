e8e47f60d6fea0d15613a588847779c226aba840
40detectives <9024463+40detectives@users.noreply.github.com>
2023-02-01 17:37:41 +0100

    feature: migrate the class playground to use use the vue tooling

--- sandbox-project/src/App.vue
+++ sandbox-project/src/App.vue
@@ -1,26 +1,71 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      incrementAmount: 8,
+      message: "Hello it works",
+      listOfItems: [
+        {
+          name: 1,
+          id: "6a887cd2-f0bf-4321-b192-92016f82a883",
+          list: [5, 6, 7],
+        },
+        {
+          name: 2,
+          id: "8d14d90b-2d47-473e-8293-d5c324111d0d",
+          list: [1, 2, 3],
+        },
+        {
+          name: 3,
+          id: "cd806d65-2309-4625-9104-dcd636cd79b5",
+          list: [1, 2, 3],
+        },
+        {
+          name: 4,
+          id: "00c939cd-fbf4-46d0-8e61-0bc2ce8a5332",
+          list: [1, 2, 3],
+        },
+        {
+          name: 5,
+          id: "295a8170-59c1-4462-9de1-3c9cd41cedab",
+          list: [1, 2, 3],
+        },
+      ],
+    };
+  },
+  methods: {
+    incrementCount(newAmount, event) {
+      console.log(newAmount);
+      console.log(event);
+      this.count += this.incrementAmount;
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Counter</h1>
+  <p :data-increment-by="incrementAmount">{{ count }}</p>
+  <button @click="incrementCount">Increment Count</button>
+  <h1>{{ incrementAmount }}</h1>
+  <div>
+    <label for="incrementAmount">Increment by:</label>
+    <input type="text" v-model="incrementAmount" />
+  </div>
+  <hr />
+  <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul v-for="item in listOfItems" v-bind:key="`item-${item.id}`">
+    <li>
+      {{ item.id }}
+      <ul>
+        <li v-for="(number, index) in item.list" v-bind:key="`number-${index}`">
+          {{ number }}
+        </li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
